131ce78f9afc0d0504e4cd00c1904cf5051fd53b
Edio.Mazera <mazera3@gmail.com>
2021-05-26 17:16:24 -0300

    versao V.3

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 require("./models/Artigo");
 const Artigo = mongoose.model('artigo');
 
 const app = express();
-
+// https://celke.com.br/artigo/consumir-dados-da-api-propria-com-react
 app.use(express.json());
+app.use((req, res, next) => {
+    // console.log("Acessou do Middleware!");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
+    next();
+    app.use(cors());
+});
+
 
 mongoose.connect('mongodb://localhost/celke', { // node_js:qwe123456@mongo_node_js:27017/node_js
     useNewUrlParser: true,
@@ -16,8 +25,8 @@ mongoose.connect('mongodb://localhost/celke', { // node_js:qwe123456@mongo_node_
 }).catch((erro) => {
     console.log("Erro: Conexão com MongoDB não foi realizada com sucesso!");
 });
-
-app.get("/", (req, res) => {
+// listar todos os rtigos
+app.get("/artigo", (req, res) => {
     Artigo.find({}).then((artigo) => {
         return res.json(artigo);
     }).catch((erro) => {
@@ -27,7 +36,56 @@ app.get("/", (req, res) => {
         })
     })
 });
-//
+// Visualizar um artigo
+//id = "60ae61b7d10295550fa903ea"
+app.get("/artigo/:id", (req, res) => {
+    //console.log(req.params.id);
+    //return res.json({id:req.params.id});
+    Artigo.findOne({
+        _id: req.params.id
+    }).then((artigo) => {
+        return res.json(artigo);
+    }).catch((erro) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado!"
+        })
+    })
+});
+// editar um artigo
+//id = "60ae61b7d10295550fa903ea"
+app.put("/artigo/:id", (req, res) => {
+    Artigo.updateOne({
+        _id: req.params.id
+    }, req.body, (err) => {
+        if (err) return res.status(400).json({
+            error: true,
+            message: "Erro: artigo não editado!"
+        });
+        return res.json({
+            error: false,
+            message: "Artigo editado com sucesso!"
+        });
+    });
+});
+// excluir um artigo
+//id = "60ae61b7d10295550fa903ea"
+app.delete("/artigo/:id", (req, res) => {
+    const artigo = Artigo.deleteOne({
+        _id: req.params.id
+    }, req.body, (err) => {
+        if (err) return res.status(400).json({
+            error: true,
+            message: "Erro: artigo não excluido!"
+        });
+        return res.json({
+            error: false,
+            message: "Artigo excluido com sucesso!"
+        });
+    });
+});
+// cadastrar um artigo via get
+//id = "60ae61b7d10295550fa903ea"
 app.get('/cad-artigo', function (req, res) {
     new Artigo({
         titulo: 'Internet 3',
@@ -38,10 +96,10 @@ app.get('/cad-artigo', function (req, res) {
         res.send('Erro. Artigo não cadastrado:' + erro)
     })
 })
-
-//
+// cadastrar um artigo via post
 app.post("/artigo", (req, res) => {
     const artigo = Artigo.create(req.body, (err) => {
+        console.log(req.body);
         if (err) return res.status(400).json({
             error: true,
             message: "Error: Artigo não foi cadastrado com sucesso!"
